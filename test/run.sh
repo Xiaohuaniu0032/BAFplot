442510e6bc73c3b35b4591cca96ff68c7c437f2a
fulongfei <lffu_0032@outlook.com>
2021-01-25 13:38:02 +0800

    update test result

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 bam='/data3/Projects/panel889/201024_A00869_0320_BHKJ5WDSXY/02_aln/201020052N.rmdup.recal.bam'
 bed="$PWD/test.bed"
 
-python ../calBAF.py -bam $bam -bed $bed -outfile MET.baf
+python ../calBAF.py -bam $bam -bed $bed -n TEST_SAMPLE -od $PWD
